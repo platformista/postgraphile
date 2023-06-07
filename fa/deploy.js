46d86f3c7b051456c5bc508ef37d458da759b707
Vincenzo Russo <v@artetecha.com>
2023-06-07 18:00:46 +0200

    That worked, but let's try that a different way

---
+++ fa/deploy.js
@@ -0,0 +1,14 @@
+// Import the requirements
+const { createAgent } = require( '@forestadmin/agent' );
+const { createSqlDataSource } = require('@forestadmin/datasource-sql');
+
+// Create your Forest Admin agent
+createAgent( {
+    // These process.env variables should be provided in the onboarding
+    authSecret: process.env.FOREST_AUTH_SECRET,
+    envSecret: process.env.FOREST_ENV_SECRET,
+    isProduction: process.env.NODE_ENV === 'production',
+} )
+    .addDataSource(createSqlDataSource(process.env.DATABASE_URL))
+    .mountOnStandaloneServer( process.env.PORT )
+    .start().then(this.stop());
